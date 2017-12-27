2a6c0fc4f4ef39fb382c5633fe2ba150c1527be4
Adam Shmeiss <34445682+adamico10@users.noreply.github.com>
2017-12-27 12:14:23 +0200

    INDIVIDUAL PROJECT EXERCISE 2

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ var posts = [];
 var postid = 1;
 
 $('.add-post').click(function() {
-    newPost = {text: $('#post-name').val(), id: postid++ }
+    newPost = {text: $('#post-name').val(), id: postid++}
     posts.push(newPost)
+    renderPost()
 })
+
+function renderPost() {
+   postid--;
+    $('.posts').append('<p class="post" data-id=' + postid +'>' + newPost.text + '</p>')
+    postid++;
+}
+ 
